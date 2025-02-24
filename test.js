fbb20688efb89a0f2b3789263bf83011d0bb5eb9
blopaz <gamegalaxyoff.pro@gmail.com>
2025-02-24 22:42:51 +0100

    Update test.js

--- test.js
+++ test.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const searchInput = document.querySelector(".search-container input");
 
     searchInput.addEventListener("focus", () => {
-        searchInput.style.width = "300px";
+        searchInput.style.width = "350px"; // Augmente encore plus
     });
 
     searchInput.addEventListener("blur", () => {
-        searchInput.style.width = "250px";
+        searchInput.style.width = "300px"; // Taille normale agrandie
     });
 });
