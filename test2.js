865a3f39ca068fb7f615a88b3ec1f23fc2e60275
blopaz <gamegalaxyoff.pro@gmail.com>
2025-02-28 11:27:57 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,10 +1,2 @@
-// Exemple de fonctionnalité JavaScript : Changement de couleur du bouton au survol
-const button = document.querySelector('.banner button');
-
-button.addEventListener('mouseover', () => {
-    button.style.backgroundColor = '#ff0000';
-});
-
-button.addEventListener('mouseout', () => {
-    button.style.backgroundColor = '#e50914';
-});
+// Affiche l'année en cours dynamiquement
+document.getElementById('year').textContent = new Date().getFullYear();
